82e857e77a7cdfebd6b2f7b869185daf57b85fff
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-07-16 20:45:14 -0600

    rewatching lecture from previous night to help understanding. getting an error on line 117 that says it is not a function. will continue to rewatch lecture and debug

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,46 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+
+let articleCreator=document.querySelector('.articles');
+articleCreator.createElement('div');
+articleCreator.classList.add('article');
+
+articleCreator.createElement('h2');
+articleCreator.textContent=('data.title');
+
+
+
+// // old newsfeed component code
+// // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+
+// class Article {
+//   constructor(domElement) {
+//     // assign this.domElement to the passed in domElement
+//     this.domElement=article;
+//     // create a reference to the ".expandButton" class. 
+//     this.expandButton=article.querySelectorAll('this.expandButton');
+//     // Using your expandButton reference, update the text on your expandButton to say "expand"
+//     this.expandButton.textContent('expand');
+//     // Set a click handler on the expandButton reference, calling the expandArticle method.
+//     this.expandButton.addEventListener('click',expandArticle.bind(this));
+//   }
+
+//   expandArticle() {
+//     // Using our reference to the domElement, toggle a class to expand or hide the article.
+//       this.domElement.classList.toggle('article-open');
+//   }
+// }
+
+// /* START HERE: 
+
+// - Select all classes named ".article" and assign that value to the articles variable.  
+
+// - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
+
+// */
+
+// let articles=document.querySelectorAll('.article');
+// articles.forEach(Article)=>{
+//   return new Article(Article)
+// };
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,24 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+// // old newsfeed component code
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu refence. 
+//   menu.classList.toggle('menu--open');
+// }
+
+// // Start Here: Create a reference to the ".menu" class
+// const menu=document.querySelector('.menu');
+// // create a reference to the ".menu-button" class
+// const menuButton=document.querySelector('.menu-button');
+// // Using your menuButton reference, add a click handler that calls toggleMenu
+// menuButton.addEventListener('click',toggleMenu);
+
+
+// new component code
+
+let menuList=document.createElement('div');
+menuList.classList.add('menu');
+
+
